22c575bd64d8223168ccfd6e54f1af5c53c2fb86
Jose Luis <joclpacheb@hotmail.com>
2021-10-06 22:27:22 -0400

    :construction: working on spin the wheel screen

--- lib/screens/ruleta.dart
+++ lib/screens/ruleta.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:blunder_store_app/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Ruleta());
@@ -10,13 +11,50 @@ class Ruleta extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: fondoDark,
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          automaticallyImplyLeading: true,
+          centerTitle: true,
+          backgroundColor: fondoDark,
+          elevation: 20,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.of(context).pop(false),
+          ),
+          title: Text('Ruleta'),
+          actions: [
+            Row(
+              children: <Widget>[
+                IconButton(
+                  padding: EdgeInsets.all(5.0),
+                  icon: Image.asset('assets/images/diamond.png'),
+                  onPressed: () {},
+                ),
+                Text(
+                  "1025",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Divider(
+                  height: 100,
+                  thickness: 20,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+              ],
+            ),
+          ],
         ),
         body: Center(
           child: Container(
-            child: Text('Aquí va la ruleta'),
+            child: Text(
+              'Aquí va la ruleta',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
